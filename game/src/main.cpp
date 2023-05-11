1f004feee145309971cdffc5d819558f44d464e7
LumiereDiou <david.asher.kingx@gmail.com>
2023-05-11 11:31:51 -0400

    Completed lab1, drew a circle

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,16 +2,42 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
+
+struct Circle
+{
+    int x = 0;
+    int y = 0;
+    int r = 0;
+    Color color;
+};
+
+void drawCircle(Circle circle)
+{
+    DrawCircle(circle.x, circle.y, circle.r, circle.color);
+}
+
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(60);
 
+    Circle circle = { 500,500, 50, DARKBLUE };
+
     while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_RIGHT)) circle.x += 2;
+        if (IsKeyDown(KEY_LEFT)) circle.x -= 2;
+        if (IsKeyDown(KEY_UP)) circle.y -= 2;
+        if (IsKeyDown(KEY_DOWN)) circle.y += 2;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+        drawCircle(circle);
+
+        //DrawLine(0, 0, 500, 500, RED);
+        //DrawRectangle(535,535, 50, 50, BLUE);
+        //DrawCircleV(circle, 50, MAROON);
+
         EndDrawing();
     }
 
