a9f31414bc64c4d0e2c92694f7b3a787c3430e39
David Asher <david.asher.kingx@gmail.com>
2023-05-30 23:00:15 -0400

    finally got the seek (when holding mouse button down) and flee to work

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,113 +1,111 @@
 #include "rlImGui.h"
 #include "Math.h"
-#include <vector>
 
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-const float deltaTime = GetFrameTime();
-
-using namespace ImGui;
-
-class RigidBody
+class Rigidbody
 {
 public:
-    Vector2 position;
-    Vector2 velocity;
-    Vector2 acceleration;
+    Vector2 position{ SCREEN_WIDTH * 0.1f, SCREEN_HEIGHT * 0.9f };
+    Vector2 velocity{};
+    Vector2 acceleration{};
 
-    RigidBody()
-        : position {100.0f,100.0f}
-        , velocity {10.0f,0.0f}
-        , acceleration{0.0f,50.0f}
+    void update(Vector2 position, Vector2 velocity, Vector2 acceleration, float deltaTime)
     {
+        this->position = position + velocity * deltaTime + acceleration * 0.5f * deltaTime * deltaTime;
+        this->velocity = velocity + acceleration * deltaTime;
+    }
 
+    float screenWrap(Vector2 position)
+    {
+        if (position.y > SCREEN_HEIGHT) { return (this->position.y = 0.0f); }
+        if (position.y < 0.0f) { return (this->position.y = SCREEN_HEIGHT); };
+        if (position.x > SCREEN_WIDTH) { return (this->position.x = 0.0f); }
+        if (position.x < 0.0f) { return (this->position.x = SCREEN_WIDTH); }
+    }
+
+    Vector2 seek(Vector2 agentPosition, Vector2 agentVelocity, Vector2 targetPosition, const float maxSpeed, const float maxAcceleration)
+    {
+        Vector2 seekDirection = Normalize(targetPosition - agentPosition);
+        Vector2 seekVelocity = seekDirection * maxSpeed;
+        Vector2 seekAcceleration = seekVelocity - agentVelocity;
+        this->acceleration = Normalize(seekAcceleration) * maxAcceleration;
+
+        return acceleration;
+    }
+
+    Vector2 flee(Vector2 agentPosition, Vector2 agentVelocity, Vector2 targetPosition, const float maxSpeed, const float maxAcceleration)
+    {
+        return seek(targetPosition, agentVelocity, agentPosition, maxSpeed, maxAcceleration);
     }
 };
 
 class Agent
 {
 public:
-    RigidBody* rigidBody;
-    float maxSpeed;
-    float maxAcceleration;
+    Rigidbody* rigidBody;
+    const float maxSpeed = 200.0f, maxAcceleration = 200.0f;
 
     Agent()
-        : maxSpeed (1000)
-        , maxAcceleration (1000)
     {
-        rigidBody = new RigidBody();
-    }
-
-    void update()
-    {
-        rigidBody->position = rigidBody->position + rigidBody->velocity * deltaTime + 0.5f * maxAcceleration * deltaTime * deltaTime;
-        rigidBody->velocity = rigidBody->velocity + maxAcceleration * deltaTime;
-    }
-        
-
-
-    Vector2 Seek(Vector2 agentPosition, Vector2 velocity, Vector2 targetPosition, float maxAcceleration)
-    {
-        Vector2 toTarget = targetPosition - agentPosition;
-        toTarget = { toTarget.x / Length(toTarget), toTarget.y / Length(toTarget) };
-        Vector2 desiredVelocity = toTarget * maxSpeed;
-        Vector2 deltaVelocity = desiredVelocity - velocity;
-        Vector2 acceleration = { deltaVelocity.x / Length(deltaVelocity), deltaVelocity.y / Length(deltaVelocity) };
-        acceleration = acceleration * maxAcceleration;
-        velocity = velocity + acceleration * deltaTime;
-        return acceleration;
-    }
-
-    Vector2 Flee(Vector2 agentPosition, Vector2 velocity, Vector2 targetPosition, float maxAcceleration)
-    {
-
-        Vector2 toTarget = targetPosition + agentPosition;
-        toTarget = { toTarget.x / Length(toTarget), toTarget.y / Length(toTarget) };
-        Vector2 desiredVelocity = toTarget * maxSpeed;
-        Vector2 deltaVelocity = desiredVelocity - velocity;
-        Vector2 acceleration = { deltaVelocity.x / Length(deltaVelocity), deltaVelocity.y / Length(deltaVelocity) };
-        acceleration = acceleration * maxAcceleration;
-        velocity = velocity + acceleration * deltaTime;
-        return acceleration;
+        rigidBody = new Rigidbody();
     }
 };
-
-
 
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    
-    SetTargetFPS(60);
-    
     rlImGuiSetup(true);
+    SetTargetFPS(60);
 
-    Agent* seeker = new Agent();
+    Agent* agent = new Agent();
     Agent* target = new Agent();
 
     while (!WindowShouldClose())
     {
-
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) target->rigidBody->position = GetMousePosition();
+        const float deltaTime = GetFrameTime();
         
-        seeker->update();
+        agent->rigidBody->update(agent->rigidBody->position, agent->rigidBody->velocity, agent->rigidBody->acceleration, deltaTime);
 
-        Vector2 seek = seeker->Seek(seeker->rigidBody->position, seeker->rigidBody->velocity,target->rigidBody->position,50);
+        agent->rigidBody->screenWrap(agent->rigidBody->position);
+
+        target->rigidBody->position = GetMousePosition();
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            agent->rigidBody->seek(agent->rigidBody->position, agent->rigidBody->velocity, target->rigidBody->position, agent->maxSpeed, agent->maxAcceleration);
+        }
+        else
+        {
+            agent->rigidBody->flee(agent->rigidBody->position, agent->rigidBody->velocity, target->rigidBody->position, agent->maxSpeed, agent->maxAcceleration);
+
+        }
+        
+        
 
         BeginDrawing();
-        
         ClearBackground(RAYWHITE);
 
-        DrawCircleV(seek, 50.0, BLUE);
-        DrawCircleV(target->rigidBody->position, 50.0, BLACK);
+        DrawCircleV(agent->rigidBody->position, 20.0f, RED);
+
+        DrawCircleV(target->rigidBody->position, 20.0f, BLUE);
+
+        rlImGuiBegin();
+        //ImGui::SliderFloat("Interpolation", &cowardice, 0.0f, 1.0f);
+        //ImGui::SliderFloat("Interpolation", &t, 0.0f, 1.0f);
+        //ImGui::SliderFloat("Seeker speed", &seekerSpeed, -100.0f, 100.0f);
+        //ImGui::SliderFloat2("Target", &target.x, 0.0f, SCREEN_WIDTH);
+        //ImGui::SliderFloat2("Position", &agent->rigidBody->position.x, 0.0f, SCREEN_WIDTH);
+        //ImGui::SliderFloat2("Velocity", &agent->rigidBody->velocity.x, -100.0f, 100.0f);
+        //ImGui::SliderFloat2("Acceleration", &acceleration.x, -100.0f, 100.0f);
+        rlImGuiEnd();
 
         EndDrawing();
     }
 
     rlImGuiShutdown();
-
     CloseWindow();
     return 0;
 }
