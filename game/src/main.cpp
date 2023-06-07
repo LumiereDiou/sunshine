f0c9ab538e78d2d95f957e7fc9fc942b9e59f771
David Asher <david.asher.kingx@gmail.com>
2023-06-07 15:41:06 -0400

    completed lab3 for single agent and object

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,76 +3,124 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-
-int main(void)
+bool CheckCollisionLineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    rlImGuiSetup(true);
-    SetTargetFPS(60);
+    Vector2 nearest = NearestPoint(lineStart, lineEnd, circlePosition);
+    return DistanceSqr(nearest, circlePosition) <= circleRadius * circleRadius;
+}
 
-
-    Vector2 position{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+class Rigidbody
+{
+public:
+    Vector2 position{};
     Vector2 velocity{};
     Vector2 acceleration{};
 
-    Vector2 target{ SCREEN_WIDTH * 0.9f, SCREEN_HEIGHT * 0.1f };
-    float seekerSpeed = 500.0f;
+    Vector2 direction{};
+    float angularSpeed;
+    float radius;
+    Color rightColor;
+    Color leftColor;
+};
 
-    // "Fully brave" by default -> seeks cursor (1 = flees cursor)
-    float bravery = 0.0f;
+void Update(Rigidbody& rb, float dt)
+{
+    rb.velocity = rb.velocity + rb.acceleration * dt;
+    rb.position = rb.position + rb.velocity * dt + rb.acceleration * dt * dt * 0.5f;
+    rb.direction = RotateTowards(rb.direction, Normalize(rb.velocity), rb.angularSpeed * dt);
+}
 
+Vector2 Seek(const Vector2& pos, const Rigidbody& rb, float maxSpeed)
+{
+    return Normalize(pos - rb.position) * maxSpeed - rb.velocity;
+}
+
+class Agent : public Rigidbody
+{
+public:
+    Rigidbody* rigidBody;
+
+    Agent()
+    {
+        rigidBody = new Rigidbody();
+    }
+    Agent(float x, float y)
+    {
+        rigidBody = new Rigidbody();
+        rigidBody->position.x = x;
+        rigidBody->position.y = y;
+    }
+
+    void ObstacleAvoidance(Agent& obstacle, Vector2 leftEnd, Vector2 rightEnd ,  float dt)
+    {
+        bool leftCollision = CheckCollisionLineCircle(position, leftEnd, obstacle.position, obstacle.radius);
+        bool rightCollision = CheckCollisionLineCircle(position, rightEnd, obstacle.position, obstacle.radius);
+        if (rightCollision)
+        {
+            Vector2 linearDirection = Normalize(velocity);
+            float linearSpeed = Length(velocity);
+            velocity = Rotate(linearDirection, -angularSpeed * dt) * linearSpeed;
+        }
+        else if (leftCollision)
+        {
+            Vector2 linearDirection = Normalize(velocity);
+            float linearSpeed = Length(velocity);
+            velocity = Rotate(linearDirection, angularSpeed * dt) * linearSpeed;
+        }
+        rightColor = rightCollision ? RED : GREEN;
+        leftColor = leftCollision ? RED : GREEN;
+    }
+};
+
+int main(void)
+{
+    Vector2 result = Normalize(Vector2{ 30.0f, 70.0f });
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Asher - Lab3");
+    rlImGuiSetup(true);
+    SetTargetFPS(60);
+
+    float seekerProbeLength = 100.0f;
+    float seekerRadius = 25.0f;
+    Agent seeker;
+    seeker.position = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    seeker.direction = { 0.0f, 1.0f };
+    seeker.angularSpeed = 100.0f * DEG2RAD;
+
+    Agent obstacle;
+    obstacle.position = { SCREEN_WIDTH * 0.75f, SCREEN_HEIGHT * 0.25f };
+    obstacle.radius = 50.0f;
+
+    bool useGUI = false;
     while (!WindowShouldClose())
     {
         const float dt = GetFrameTime();
-        //position = position + velocity * dt + acceleration * 0.5f * dt * dt;
-        velocity = velocity + acceleration * dt;
-        if (position.y > SCREEN_HEIGHT) position.y = 0.0f;
-        if (position.y < 0.0f) position.y = SCREEN_HEIGHT;
-        if (position.x > SCREEN_WIDTH) position.x = 0.0f;
-        if (position.x < 0.0f) position.x = SCREEN_WIDTH;
 
-        target = GetMousePosition();
+        Vector2 right = Rotate(seeker.direction, 30.0f * DEG2RAD);
+        Vector2 left = Rotate(seeker.direction, -30.0f * DEG2RAD);
+        Vector2 rightEnd = seeker.position + right * seekerProbeLength;
+        Vector2 leftEnd = seeker.position + left * seekerProbeLength;
 
-        // AB = B - A
-        Vector2 seekDirection = Normalize(target - position);
-        Vector2 seekVelocity = seekDirection * seekerSpeed;
-        Vector2 seekAcceleration = seekVelocity - velocity;
+        Vector2 right2 = Rotate(seeker.direction, 210.0f * DEG2RAD);
+        Vector2 left2 = Rotate(seeker.direction, -210.0f * DEG2RAD);
+        Vector2 rightEnd2 = seeker.position + right2 * seekerProbeLength;
+        Vector2 leftEnd2 = seeker.position + left2 * seekerProbeLength;
 
-        Vector2 fleeDirection = Normalize(position - target);
-        Vector2 fleeVelocity = fleeDirection * seekerSpeed;
-        Vector2 fleeAcceleration = fleeVelocity - velocity;
+        seeker.acceleration = Seek(GetMousePosition(), seeker, 100.0f);
+        Update(seeker, dt);
         
-        
-        Vector2 angle = Normalize(seekDirection);
-        float angleFromVector = atan2f(angle.y, angle.x);
-        angleFromVector = fmodf(angleFromVector + 2 * PI, 2 * PI);
-
-        float angle30DegreesFromVector = angleFromVector - 15 * DEG2RAD;
-        angle30DegreesFromVector = fmodf(angle30DegreesFromVector + 2 * PI, 2 * PI);
-        Vector2 vectorAt30Degrees = Vector2{ cosf(angle30DegreesFromVector), sinf(angle30DegreesFromVector) };
-
-        acceleration = Lerp(seekAcceleration, fleeAcceleration, bravery);
+        seeker.ObstacleAvoidance(obstacle, leftEnd, rightEnd, dt);
+        seeker.ObstacleAvoidance(obstacle, leftEnd2, rightEnd2, dt);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircleV(position, 20.0f, RED);
-        DrawCircleV(target, 20.0f, BLUE);
-        //DrawLineV(position, position + 100.0f, BLACK);
-        DrawLineV(position, position + fleeDirection * 100.0f, BLACK);
-       
-        DrawLineV(position, position + angle * 100.0f, GREEN);
-        DrawLineV(position, position + vectorAt30Degrees * 100.0f, GREEN);
+        DrawCircleV(seeker.position, seekerRadius, BLUE);
+        DrawCircleV(obstacle.position, obstacle.radius, GRAY);
+        DrawLineV(seeker.position, rightEnd, seeker.rightColor);
+        DrawLineV(seeker.position, leftEnd, seeker.leftColor);
 
-
-        rlImGuiBegin();
-        //ImGui::SliderFloat("Interpolation", &bravery, 0.0f, 1.0f);
-        //ImGui::SliderFloat("Interpolation", &t, 0.0f, 1.0f);
-        //ImGui::SliderFloat("Seeker speed", &seekerSpeed, -100.0f, 100.0f);
-        //ImGui::SliderFloat2("Target", &target.x, 0.0f, SCREEN_WIDTH);
-        //ImGui::SliderFloat2("Position", &position.x, 0.0f, SCREEN_WIDTH);
-        //ImGui::SliderFloat2("Velocity", &velocity.x, -100.0f, 100.0f);
-        //ImGui::SliderFloat2("Acceleration", &acceleration.x, -100.0f, 100.0f);
-        rlImGuiEnd();
+        DrawLineV(seeker.position, rightEnd2, seeker.rightColor);
+        DrawLineV(seeker.position, leftEnd2, seeker.leftColor);
 
         EndDrawing();
     }
